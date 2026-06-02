66435978f0a128b69eadf9277215f2c5e8597514
MuhammadAbsarSiddiqui <absar.sid359@gmail.com>
2026-06-02 15:01:42 +0500

    feature: changed first line differently

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// app.js
-console.log("Welcome to the 360 Attendance App");
-console.log("FEATURE VERSION - This is from feature branch");
-// app.js
+console.log("FEATURE VERSION - Hello from feature branch");
 console.log("Welcome to the 360 Attendance App");
 console.log("Loading new login button...");
-
