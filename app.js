d1237bc797cb2566ef8446e67760e51d508edd67
MuhammadAbsarSiddiqui <absar.sid359@gmail.com>
2026-06-02 14:34:12 +0500

    feature: changed welcome message

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // app.js
 console.log("Welcome to the 360 Attendance App");
-
+console.log("FEATURE VERSION - This is from feature branch");
 // app.js
 console.log("Welcome to the 360 Attendance App");
 console.log("Loading new login button...");
